7243d28b7961e08dd98e8c311bbe33b0ef12cca5
Nyi-Nyi-3756 <nyinyihtun3741@gmail.com>
2025-10-05 11:17:12 +0700

    update assignment1

---
+++ src/main/java/assignment01/assignment01.java
@@ -0,0 +1,20 @@
+package main.java.assignment01;
+
+public class assignment01 {
+    public static void main(String[] args) {
+        // Variables
+        String name = "Nyi Nyi Tun";
+        int age = 25;
+        String city = "Yangon";
+        String hobby = "Coding and Design";
+        boolean student = true;
+
+        // Create a paragraph using variables
+        String paragraph = "Hello, my name is " + name + ". I am " + age + " years old and I live in "
+                + city + ". My favorite hobby is " + hobby + ". "
+                + "Currently, am I a student? The answer is " + student + ".";
+
+        // Print paragraph
+        System.out.println(paragraph);
+    }
+}
